28f8f6486e2131188ec877e0a242d25b65d33ef6
DavidO28 <damers22@yahoo.com>
2025-01-28 12:31:24 +0400

    adjusted css and made conditional rendering

--- src/components/AspectRatio.vue
+++ src/components/AspectRatio.vue
@@ -1,27 +1,28 @@
 <template>
-  <v-label class="mb-2">Image Aspect Ratio</v-label>
-  <v-divider class="mb-4" />
-  <div class="d-flex flex-wrap align-center justify-center">
-    <div
-      class="button-size ma-1 pa-4 flex-row align-center rounded border-primary text-caption aspect-ratio-chip font-weight-bold landscape"
-      :class="aspectRatio === 'landscape' ? 'selected' : 'unselected'"
-      @click="setAspectRatio('landscape')"
-    >
-      16:9 Landscape
-    </div>
-    <div
-      class="button-size pa-4 ma-1flex-row align-center rounded border-primary text-caption aspect-ratio-chip font-weight-bold square"
-      :class="aspectRatio === 'square' ? 'selected' : 'unselected'"
-      @click="setAspectRatio('square')"
-    >
-      1:1 Square
-    </div>
-    <div
-      class="button-size pa-4 ma-1 flex-row align-center rounded border-primary text-caption aspect-ratio-chip font-weight-bold portrait"
-      :class="aspectRatio === 'portrait' ? 'selected' : 'unselected'"
-      @click="setAspectRatio('portrait')"
-    >
-      9:16 Portrait
+  <div
+    class="h-screen container d-flex flex-column justify-center align-center text-center"
+  >
+    <v-label class="mb-2 font-weight-black">Choose Image Aspect Ratio</v-label>
+    <v-divider class="mb-4" />
+    <div class="d-flex flex-wrap align-center justify-center">
+      <div
+        class="button ma-3 pa-4 flex-row align-center rounded border-primary text-caption aspect-ratio-chip font-weight-bold landscape"
+        @click="setAspectRatio('landscape')"
+      >
+        16:9 Landscape
+      </div>
+      <div
+        class="button pa-4 ma-3 flex-row align-center rounded border-primary text-caption aspect-ratio-chip font-weight-bold square"
+        @click="setAspectRatio('square')"
+      >
+        1:1 Square
+      </div>
+      <div
+        class="button pa-4 ma-3 flex-row align-center rounded border-primary text-caption aspect-ratio-chip font-weight-bold portrait"
+        @click="setAspectRatio('portrait')"
+      >
+        9:16 Portrait
+      </div>
     </div>
   </div>
 </template>
@@ -31,7 +32,7 @@
   import { ref } from 'vue'
 
   const cardStore = useCardStore()
-  const aspectRatio = ref('landscape')
+  const aspectRatio = ref('')
 
   const setAspectRatio = (ratio: string) => {
     aspectRatio.value = ratio
@@ -41,34 +42,19 @@
       portrait: [566, 1080],
     }
     if (dimensions[ratio]) {
-      [cardStore.width, cardStore.height] = dimensions[ratio]
+      ;[cardStore.width, cardStore.height] = dimensions[ratio]
     }
   }
 </script>
 
 <style scoped>
-  .selected {
-    border: 2px solid #2d04f7;
-    cursor: pointer;
+  .container {
+    background-image: linear-gradient(
+      105.5deg,
+      rgba(31, 212, 248, 1) 11%,
+      rgba(218, 15, 183, 1) 74.9%
+    );
   }
-
-  .unselected {
-    border: 2px solid #8e4e6e;
-    cursor: pointer;
-  }
-
-  .selected-bg-color {
-    background-color: #2d04f7;
-  }
-
-  .caption-style {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    background-color: rgba(0, 0, 0, 0.7);
-    padding: 10rem;
-  }
-
   .aspect-ratio-chip {
     display: inline-flex;
     align-items: center;
@@ -79,11 +65,8 @@
   .aspect-ratio-chip::before {
     content: '';
     margin-right: 0.5rem;
-    background-color: #8e4e6e;
-  }
-
-  .aspect-ratio-chip.selected::before {
-    background-color: #2d04f7;
+    background-color: white;
+    transition: 0.3s all;
   }
 
   .aspect-ratio-chip.landscape::before {
@@ -102,14 +85,22 @@
     height: 1.6rem;
   }
 
-  .style-card-title {
-    border-top-right-radius: 8rem;
-    bottom: 0;
-    left: 0;
+  .button {
+    width: 200px !important;
+    height: 70px !important;
+    border: 2px solid white;
+    color: white;
+    cursor: pointer;
+    transition: 0.3s all;
   }
 
-  .button-size {
-    width: 200px !important;
-    height: 50px !important;
+  .button:hover {
+    border: 2px solid greenyellow;
+    color: greenyellow;
+    transform: scale(1.1);
+  }
+
+  .button:hover.aspect-ratio-chip::before {
+    background-color: greenyellow;
   }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
-    <AspectRatio />
-    <Frame />
+    <AspectRatio v-if="!cardStore.width && !cardStore.height" />
+    <Frame v-else />
   </div>
 </template>
 
 <script setup lang="ts">
   import AspectRatio from '@/components/AspectRatio.vue'
   import Frame from '@/components/Frame.vue'
+  import { useCardStore } from '@/store/card'
+
+  const cardStore = useCardStore()
 </script>
