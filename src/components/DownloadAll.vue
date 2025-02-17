cfd7dc2b3f382cb1400616f9b8756744a1dadc03
DavidO28 <damers22@yahoo.com>
2025-02-17 12:06:14 +0400

    simplified download component

--- src/components/DownloadAll.vue
+++ src/components/DownloadAll.vue
@@ -1,17 +1,9 @@
 <template>
   <div class="mt-2 d-flex justify-center">
-    <v-btn
-      v-if="cardStore.frame.length > 1"
-      @click="downloadAllImages"
-    >
-      Download all images
-    </v-btn>
+    <v-btn @click="downloadAllImages">Download all images</v-btn>
   </div>
 </template>
 
 <script setup lang="ts">
-  import { useCardStore } from '@/store/card'
   import { downloadAllImages } from '@/composables/useDownload'
-
-  const cardStore = useCardStore()
 </script>
