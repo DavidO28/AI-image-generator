0fcba97dd224541b3977a4b1f3db8dd1f40a77fd
DavidO28 <damers22@yahoo.com>
2025-02-19 10:44:06 +0400

    updated styles

--- src/components/ThemeSwitcher.vue
+++ src/components/ThemeSwitcher.vue
@@ -3,7 +3,10 @@
     v-for="frameItem in cardStore.frame"
     :key="frameItem.id"
   >
-    <select v-model="frameItem.theme">
+    <select
+      v-model="frameItem.theme"
+      class="custom-select"
+    >
       <option
         v-for="(theme, id) in cardStore.themeOptions"
         :key="id"
@@ -33,3 +36,19 @@
     { immediate: true },
   )
 </script>
+
+<style scoped>
+  .custom-select {
+    width: 100%;
+    padding: 10px;
+    font-size: 16px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    background-color: white;
+  }
+
+  .custom-select:focus {
+    border-color: #007bff;
+    outline: none;
+  }
+</style>
