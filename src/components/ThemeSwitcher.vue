1ff254bb181e7c2098e8e3cd408d1ada1b139f0f
DavidO28 <damers22@yahoo.com>
2025-02-25 12:18:28 +0400

    refactored code

--- src/components/ThemeSwitcher.vue
+++ src/components/ThemeSwitcher.vue
@@ -1,40 +1,29 @@
 <template>
-  <div>
-    <select
-      name="theme-selector"
-      v-model="selectedTheme"
-      class="custom-select pa-1 border-primary rounded border-md"
+  <select
+    v-model="selectedTheme"
+    class="custom-select pa-1 border-primary rounded border-md"
+  >
+    <option
+      v-for="theme in cardStore.themeOptions"
+      :key="theme"
+      :value="theme"
     >
-      <option
-        v-for="(theme, id) in cardStore.themeOptions"
-        :key="id"
-        :value="theme"
-      >
-        {{ theme }}
-      </option>
-    </select>
-  </div>
+      {{ theme }}
+    </option>
+  </select>
 </template>
 
 <script setup lang="ts">
   import { useCardStore } from '@/store/card'
-  import { onMounted, ref, watch } from 'vue'
+  import { ref, watch } from 'vue'
 
   const cardStore = useCardStore()
-  const selectedTheme = ref('')
-
-  onMounted(() => {
-    selectedTheme.value = 'Theme: Realistic style'
-  })
+  const selectedTheme = ref('Theme: Realistic style')
 
   watch(
-    () => selectedTheme.value,
+    selectedTheme,
     (newTheme) => {
-      if (newTheme) {
-        cardStore.frame.forEach((frameItem) => {
-          frameItem.theme = newTheme
-        })
-      }
+      cardStore.frame.forEach((frameItem) => (frameItem.theme = newTheme))
     },
     { immediate: true },
   )
