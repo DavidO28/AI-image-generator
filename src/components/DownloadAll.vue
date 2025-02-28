8eb87d30ccd8681737dd33b8588e3a086abdfb32
DavidO28 <damers22@yahoo.com>
2025-02-28 12:22:53 +0400

    added dist

--- src/components/DownloadAll.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <v-btn @click="downloadAllImages">Download all images</v-btn>
-</template>
-
-<script setup lang="ts">
-  import { downloadAllImages } from '@/composables/useDownload'
-</script>
